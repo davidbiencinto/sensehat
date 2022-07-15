921adea1f562142aa0845e9f9ab3df57c70e45d8
DavidBiencinto <david.biencinto@gmail.com>
2022-07-15 16:43:14 +0200

    +

--- demo.py
+++ demo.py
@@ -2,8 +2,8 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
-X = [255, 255, 0]  # Red
-O = [100, 150, 100]  # White
+X = [255, 0, 255]  # Red
+O = [20, 20, 20]  # White
 
 question_mark = [
 O, O, O, X, X, O, O, O,
