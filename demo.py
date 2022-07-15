5adf133d392ff75b678f7fd6c766717d53f9dfe0
DavidBiencinto <david.biencinto@gmail.com>
2022-07-15 16:53:07 +0200

    p

--- demo.py
+++ demo.py
@@ -3,6 +3,20 @@ import time
 
 sense = SenseHat()
 
+X = [0, 0, 0]  # Red
+O = [0, 0, 0]  # White
+
+clear = [
+O, O, O, X, X, O, O, O,
+O, O, X, O, O, X, O, O,
+O, O, O, O, O, X, O, O,
+O, O, O, O, X, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, X, O, O, O, O
+]
+
 X = [255, 0, 255]  # Red
 O = [20, 20, 20]  # White
 
@@ -31,7 +45,9 @@ O, O, O, O, O, O, O, O,
 O, O, O, X, O, O, O, O
 ]
 
+sense.set_pixels(clear)
+
 for x in range(5):
     sense.set_pixels(question_mark)
-    time.sleep(3)
+    time.sleep(1)
     sense.set_pixels(hearth)
